e99f0efc8a90e7894e5e4e53adfebda5fbdaae74
Cao Minh Tu <caominhtuvn@gmail.com>
2024-01-07 15:44:33 +0700

    Viết hàm getComputerChoice trả về giá trị ngẫu nhiên trong mảng đầu vào

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const cácGiáTrị = ['Kéo', 'Búa', 'Bao'];
+
+function getComputerChoice(cácGiáTrị) {
+    /* Trả về giá trị ngẫu nhiên trong mảng các giá trị đầu vào */
+    return cácGiáTrị[Math.floor(Math.random() * cácGiáTrị.length)];
+}
+
